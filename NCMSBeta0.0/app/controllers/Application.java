3c01f99f3c39c65e887c701a25fb96b5d2572079
narendra-egnaroinc <narendrar@egnaroinc.com>
2014-12-28 17:45:22 +0530

    Changed Application Controller

--- NCMSBeta0.0/app/controllers/Application.java
+++ NCMSBeta0.0/app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
   
   public static Result index() {
-	  
+	  System.out.println("Testing with GIT");
     return ok(index.render("Your new application is ready."));
   }
   
